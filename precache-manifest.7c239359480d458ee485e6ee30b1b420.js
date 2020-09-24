c539092c8a920a27068a7b5f3bd53031c3d9b81b
cieslikprzemyslaw <cprzemek91@hotmail.com>
2020-09-24 22:21:56 +0100

    Updates

---
+++ precache-manifest.7c239359480d458ee485e6ee30b1b420.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7b82d168ccd2071606671994271514c",
+    "url": "/weatherApp-metofficeApi/index.html"
+  },
+  {
+    "revision": "a99cbcf57f99303d0c16",
+    "url": "/weatherApp-metofficeApi/static/css/main.ed48690a.chunk.css"
+  },
+  {
+    "revision": "cbc128854f765730db4f",
+    "url": "/weatherApp-metofficeApi/static/js/2.bbf0a126.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherApp-metofficeApi/static/js/2.bbf0a126.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a99cbcf57f99303d0c16",
+    "url": "/weatherApp-metofficeApi/static/js/main.9d532150.chunk.js"
+  },
+  {
+    "revision": "dae3ad6641b0489e5f4d",
+    "url": "/weatherApp-metofficeApi/static/js/runtime-main.6b3fccbd.js"
+  }
+]);
